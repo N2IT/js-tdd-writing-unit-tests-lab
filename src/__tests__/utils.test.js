bdc1a73a6d8bc4464b33872feb8c70d87cdcd223
Tony Eder <tonyeder11@gmail.com>
2024-01-05 11:13:02 -0800

    green stage reached

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,13 @@
 // Your tests here
+import { isPalindrome } from '../utils'
+
+describe("isPalindrome", () => {
+    it("calculates double the points if the word is a palindrome", () => {
+        //arrange
+        const word = "mom"
+        const check = isPalindrome(word)
+
+        //assert
+        expect(check).toBe(true)
+    })
+})
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,13 @@
 // Your code here
+export function isPalindrome(word){
+    let forward = word
+    let reverse = word.split("").reverse().join("")
+    console.log(forward)
+    console.log(reverse)
+
+    if(forward === reverse){
+        return true
+    } else {
+        return false
+    }
+}
